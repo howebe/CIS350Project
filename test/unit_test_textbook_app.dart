e85bfa56bd6a0918ca7f0a273607a5eb91ec0018
Brandon Baker <brandonbaker@Brandons-MacBook-Air.local>
2022-12-03 15:09:23 -0500

    created new file for unit tests

---
+++ test/unit_test_textbook_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+void main() {
+  
+setUp(() {
+  null;
+});
+
+test('Test initial values', () {
+  return null;
+});
+
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,20 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:textbook_sharing_app/main.dart';
+import 'package:textbook_sharing_app/main.dart';
 
 void main() {
+
   testWidgets('Test Welcome Page', (WidgetTester tester) async {
   //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(const MyApp());
+  
+    //await tester.pumpWidget(const MyApp());
 
-  //   // Verify that our counter starts at 0.
-  //   expect(find.text('10'), findsOneWidget);
-  //   expect(find.text('1'), findsNothing);
+    // expect(find.text('Continue without signing in'), findsOneWidget);
+    // expect(find.text('Login or Register'), findsOneWidget);
 
-  //   // Tap the '+' icon and trigger a frame.
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-
-  //   // Verify that our counter has incremented.
-  //   expect(find.text('0'), findsNothing);
-  //   expect(find.text('1'), findsOneWidget);
   });
 }
